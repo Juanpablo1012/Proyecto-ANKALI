813872fd7303d456d5ea5c2560b605f4db31591e
Kluna1215 <karenlunavasquez@gmail.com>
2021-04-13 11:32:53 -0500

    ok

--- Vista/Usuariosadmin.php
+++ Vista/Usuariosadmin.php
@@ -10,7 +10,7 @@ $listarusuarios = json_decode($ControladorUsuarios->Listarusuarios());
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ANKALI</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
@@ -136,17 +136,7 @@ $listarusuarios = json_decode($ControladorUsuarios->Listarusuarios());
                     <td><?php echo $usu->Nombre?></td>
                     <td><?php echo $usu->Correo?></td>
                     <td><?php echo $usu->Telefono?></td>
-                    <td>
-                    <?php 
-                        if($usu->NombreRol == 'U' ){
-                          echo "Usuario";
-                        }
-                        else if($usu->NombreRol == 'A' )
-                        {
-                          echo "Administrador";
-                        }
-                   ?>
-                  </td>
+                    <td><?php echo $usu->NombreRol?></td>
                     <td>
                     <?php if($usu->Estado == 1 ){
                         echo "<h4 style='color:green;'> Activo </h4>";
