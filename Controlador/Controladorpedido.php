a473ac1b4368b59c88659c86a6e4f5e2b4c60df4
Kluna1215 <karenlunavasquez@gmail.com>
2021-06-16 07:25:50 -0500

    pedido

--- Controlador/Controladorpedido.php
+++ Controlador/Controladorpedido.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>ANKALI</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-        <link href="//cdn.jsdelivr.net/npm/@sweetalert2/theme-dark@4/dark.css" rel="stylesheet">
-        <link href="../Estilo/css/style.css" rel="stylesheet">
-        <link href="../Estilo/img/logo-negro.png" rel="icon">
-        <link href="../Estilo/img/apple-touch-icon.png" rel="apple-touch-icon">
-        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
-
-    </head>
-    <body>
-        
-    </body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-
-</html><?php
+<?php
 // session_start();
 require_once("../Modelo/Conexion.php");
 require_once("../Modelo/Usuarios.php");
